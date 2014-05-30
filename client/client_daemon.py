9ba2a011c7198744a259f05ee0010d91ff900e3e
FedeDR <dellarovere.federico@gmail.com>
2014-05-30 18:04:46 +0200

    add server communicato for server interface

--- client/client_daemon.py
+++ client/client_daemon.py
@@ -13,6 +13,18 @@ import requests
 import json
 
 
+class ServerCommunicator(object):
+
+    def __init__(self, server_url):
+        self.server_url = server_url
+
+    def put(self, dst_path):
+        r = requests.get(self.server_url)
+
+    def delete(self, dst_path):
+        r = requests.get(self.server_url)
+
+
 def load_config():
     with open('config.json', 'r') as config_file:
         config = json.load(config_file)
@@ -21,16 +33,59 @@ def load_config():
 
 class DirectoryEventHandler(FileSystemEventHandler):
 
-    def __init__(self, server_url):
-        self.server_url = server_url
+    def __init__(self, cmd):
+        self.cmd = cmd
 
-    def on_any_event(self, event):
-        request = requests.get(self.server_url)
+    # def on_any_event(self, event):
+        # request = requests.get(self.server_url)
+
+    def on_moved(self, event):
+        """Called when a file or a directory is moved or renamed.
+
+        :param event:
+            Event representing file/directory movement.
+        :type event:
+            :class:`DirMovedEvent` or :class:`FileMovedEvent`
+        """
+        self.cmd.delete(event.src_path)
+        self.cmd.put(event.dest_path)
+
+    def on_created(self, event):
+        """Called when a file or directory is created.
+
+        :param event:
+            Event representing file/directory creation.
+        :type event:
+            :class:`DirCreatedEvent` or :class:`FileCreatedEvent`
+        """
+        self.cmd.put(event.src_path)
+
+    def on_deleted(self, event):
+        """Called when a file or directory is deleted.
+
+        :param event:
+            Event representing file/directory deletion.
+        :type event:
+            :class:`DirDeletedEvent` or :class:`FileDeletedEvent`
+        """
+        self.cmd.delete(event.src_path)
+
+    def on_modified(self, event):
+        """Called when a file or directory is modified.
+
+        :param event:
+            Event representing file/directory modification.
+        :type event:
+            :class:`DirModifiedEvent` or :class:`FileModifiedEvent`
+        """
+        if not event.is_directory:
+            self.cmd.put(event.src_path)
 
 
 def main():
     config = load_config()
-    event_handler = DirectoryEventHandler(config['server_url'])
+    server_com = ServerCommunicator(config['server_url'])
+    event_handler = DirectoryEventHandler(server_com)
     observer = Observer()
     observer.schedule(event_handler, config['dir_path'], recursive=True)
     observer.start()
