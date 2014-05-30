8eb0e8448bdb61dd67541937ab8489cfc647158c
server side table 1 <emilioveloci@gmail.com>
2014-05-30 15:17:22 +0200

    not authenticated and authenticated request examples + create_user method

--- server/server.py
+++ server/server.py
@@ -1,2 +1,50 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+
+import datetime
+from flask import Flask
+from flask import request
+from flask.ext.httpauth import HTTPBasicAuth
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+# username : password
+users = {
+	"testUser" : "propropro",
+}
+
+@auth.get_password
+def get_pw(username):
+	print username
+	if username in users:
+		return users[username]
+	return None
+
+@app.route("/hidden_page")
+@auth.login_required
+def hidden_page():
+	return "Hello {}".format(auth.username())
+
+@app.route("/create_user", methods=["POST"])
+# this method takes only 'user' and 'psw' as POST variables
+def create_user():
+	if not ("user" in request.form and "psw" in request.form):
+		return "400 Bad Request"
+	if request.form["user"] in users:
+		return "Error! Another user has your nickname\n" 	# TODO: code?
+	
+	users[request.form["user"]] = request.form["psw"]
+	return "User created!\n"
+
+@app.route("/")
+def welcome():
+	local_time = datetime.datetime.now()
+	formatted_time = local_time.strftime("%Y-%m-%d %H:%M")
+	return "Welcome on the Server!\n{}\n".format(formatted_time)
+
+def main():
+	app.run()
+
+if __name__ == '__main__':
+	main()
