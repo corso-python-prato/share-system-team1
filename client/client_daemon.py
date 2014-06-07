2b5be6086b0141e0fa4d3383dcdcb6901460737b
Marco Pretelli <lordscapoladestra@gmail.com>
2014-06-07 18:06:31 +0200

    add httpbasic auth

--- client/client_daemon.py
+++ client/client_daemon.py
@@ -8,120 +8,124 @@
 # from watchdog.observers import Observer
 from watchdog.observers.polling import PollingObserver as Observer
 from watchdog.events import FileSystemEventHandler
-import time
+from requests.auth import HTTPBasicAuth
 import requests
+import time
 import json
 
 def req_post(*args, **kwargs):
-    return requests.post(*args, **kwargs)
+	return requests.post(*args, **kwargs)
 
 
 class ServerCommunicator(object):
 
-    def __init__(self, server_url):
-        self.server_url = server_url
+	def __init__(self, server_url):
+		self.server_url = server_url
 
-    """   
-    def get(self, dst_path):
-        r = requests.get(self.server_url)
-    """
+	def get(self, dst_path):
+		r = requests.get(self.server_url)
+	
+	def post(self, dst_path, login_credential):
+		file_name = dst_path.split('/')[-1]
+		file_content = ''
+		
+		try:
+			with open("dst_path", 'r') as f:
+				file_content =  f.read()
+		except IOError:
+			#Atomic create and delete error!
+			return False
+			
+		upload = {
+					'file_name': file_name,
+					'file_content': file_content
+				 }
+		auth = HTTPBasicAuth(
+					login_credential['user'],
+					login_credential['psw'])
 
-    def post(self, dst_path, login_credential):
-        file_name = dst_path.split('/')[-1]
-        file_content = ''
-        
-        with open(dst_path, 'r') as f:
-            file_content =  f.read()
+		server_url = "{}/files/{}".format(self.server_url, dst_path)
 
-        upload = {
-                    'username': login_credential['user'],
-                    'password': login_credential['psw'],
-                    'file_name': file_name,
-                    'file_content': file_content
-                 }
+		req_post(server_url, data = upload, auth = auth)
 
-        server_url = "{}/files/{}".format(self.server_url, dst_path)
- 
-        req_post(server_url, upload)
+	def put(self, dst_path):
+		r = requests.get(self.server_url)
 
-    def put(self, dst_path):
-        r = requests.get(self.server_url)
-
-    def delete(self, dst_path):
-        r = requests.get(self.server_url)
+	def delete(self, dst_path):
+		r = requests.get(self.server_url)
 
 
 def load_config():
-    with open('config.json', 'r') as config_file:
-        config = json.load(config_file)
-    return config
+	with open('config.json', 'r') as config_file:
+		config = json.load(config_file)
+	return config
 
 
 class DirectoryEventHandler(FileSystemEventHandler):
 
-    def __init__(self, cmd):
-        self.cmd = cmd
-        self.credential = {"psw": "marco", "user": "marco"}
+	def __init__(self, cmd):
+		self.cmd = cmd
+		self.credential = {"psw": "marco", "user": "marco"}
 
-    # def on_any_event(self, event):
-        # request = requests.get(self.server_url)
+	# def on_any_event(self, event):
+		# request = requests.get(self.server_url)
 
-    def on_moved(self, event):
-        """Called when a file or a directory is moved or renamed.
+	def on_moved(self, event):
+		"""Called when a file or a directory is moved or renamed.
 
-        :param event:
-            Event representing file/directory movement.
-        :type event:
-            :class:`DirMovedEvent` or :class:`FileMovedEvent`
-        """
-        self.cmd.delete(event.src_path)
-        self.cmd.post(event.dest_path)
+		:param event:
+			Event representing file/directory movement.
+		:type event:
+			:class:`DirMovedEvent` or :class:`FileMovedEvent`
+		"""
+		self.cmd.delete(event.src_path)
+		self.cmd.post(event.dest_path)
 
-    def on_created(self, event):
-        """Called when a file or directory is created.
+	def on_created(self, event):
+		"""Called when a file or directory is created.
 
-        :param event:
-            Event representing file/directory creation.
-        :type event:
-            :class:`DirCreatedEvent` or :class:`FileCreatedEvent`
-        """
-        self.cmd.post(event.src_path, self.credential)
+		:param event:
+			Event representing file/directory creation.
+		:type event:
+			:class:`DirCreatedEvent` or :class:`FileCreatedEvent`
+		"""
+		self.cmd.post(event.src_path, self.credential)
 
-    def on_deleted(self, event):
-        """Called when a file or directory is deleted.
+	def on_deleted(self, event):
+		"""Called when a file or directory is deleted.
 
-        :param event:
-            Event representing file/directory deletion.
-        :type event:
-            :class:`DirDeletedEvent` or :class:`FileDeletedEvent`
-        """
-        self.cmd.delete(event.src_path)
+		:param event:
+			Event representing file/directory deletion.
+		:type event:
+			:class:`DirDeletedEvent` or :class:`FileDeletedEvent`
+		"""
+		self.cmd.delete(event.src_path)
 
-    def on_modified(self, event):
-        """Called when a file or directory is modified.
+	def on_modified(self, event):
+		"""Called when a file or directory is modified.
 
-        :param event:
-            Event representing file/directory modification.
-        :type event:
-            :class:`DirModifiedEvent` or :class:`FileModifiedEvent`
-        """
-        if not event.is_directory:
-            self.cmd.put(event.src_path)
+		:param event:
+			Event representing file/directory modification.
+		:type event:
+			:class:`DirModifiedEvent` or :class:`FileModifiedEvent`
+		"""
+		if not event.is_directory:
+			self.cmd.put(event.src_path)
 
 
 def main():
-    config = load_config()
-    server_com = ServerCommunicator(config['server_url'])
-    event_handler = DirectoryEventHandler(server_com)
-    observer = Observer()
-    observer.schedule(event_handler, config['dir_path'], recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+	config = load_config()
+	server_com = ServerCommunicator(config['server_url'])
+	event_handler = DirectoryEventHandler(server_com)
+	observer = Observer()
+	observer.schedule(event_handler, config['dir_path'], recursive=True)
+	observer.start()
+	try:
+		while True:
+			time.sleep(1)
+	except KeyboardInterrupt:
+		observer.stop()
+	observer.join()
 
 if __name__ == '__main__':
-    main()
+	main()
--- unitTest/client/client_daemon.py
+++ unitTest/client/client_daemon.py
@@ -8,59 +8,70 @@ import client_daemon
 
 @all_requests
 def response_201(url, request):
-    """Upload OK"""
+	"""Upload OK"""
 
-    return {'status_code': 201}
+	return {'status_code': 201}
 
 def response_300(url, request):
-    """Access Denied"""
-    return {'status_code': 300}
+	"""Access Denied"""
+	return {'status_code': 300}
 
 
 class ClientDaemonTest(unittest.TestCase):
-    def setup(self):
-    	pass
+	def setup(self):
+		pass
 
-    def test_correct_data_format_post(self):
-        def mock_req_post(*args, **kwargs):
-            global content
-            content = args
+	def test_upload(self):
+		def mock_req_post(*args, **kwargs):
+			global upload_auth, upload_data, upload_url
+			upload_auth = kwargs['auth']
+			upload_data = kwargs['data']
+			upload_url = args[0]
 
-        client_daemon.req_post = mock_req_post
-        login = {"psw": "marco", "user": "marco"}
-        path = "mock_file/prova.txt"
-        mock_result = (
-            'http://www.mioserver.it/files/mock_file/prova.txt', 
-            {
-                'username': 'marco', 
-                'file_name': 'prova.txt', 
-                'password': 'marco', 
-                'file_content': 'LOREM IPSIUM!'
-            })
+		client_daemon.req_post = mock_req_post
 
-        client_daemon.ServerCommunicator("http://www.mioserver.it").post(path, login)
-        self.assertEqual(content, mock_result) 
+		login = {"psw": "pswMarco", "user": "userMarco"}
+		path = "mock_file/prova.txt"
+		
+		mock_auth_user = 'userMarco'
+		mock_auth_psw = 'pswMarco'
+		mock_url = 'http://www.mioserver.it/files/mock_file/prova.txt' 
+		mock_data = {
+				'file_name': 'prova.txt', 
+				'file_content': 'LOREM IPSIUM!'
+			}
 
-    def test_put(self):
-    	pass
+		client_daemon.ServerCommunicator("http://www.mioserver.it").post(path, login)
 
-    def test_delete(self):
-    	pass
+		#check if username is equals
+		self.assertEqual(upload_auth.username, mock_auth_user)
+		#check if password is equals
+		self.assertEqual(upload_auth.password, mock_auth_psw)
+		#check if data is equals
+		self.assertEqual(upload_data, mock_data) 
+		#check if url is equals
+		self.assertEqual(upload_url, mock_url)   
 
-    def test_on_moved(self):
-    	pass
+	def test_put(self):
+		pass
 
-    def test_on_created(self):
-    	pass
+	def test_delete(self):
+		pass
 
-    def test_on_deleted(self):
-    	pass
+	def test_on_moved(self):
+		pass
 
-    def test_on_modified(self):
-    	pass
+	def test_on_created(self):
+		pass
 
-        
+	def test_on_deleted(self):
+		pass
+
+	def test_on_modified(self):
+		pass
+
+		
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
 
