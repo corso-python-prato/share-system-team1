b5203c1c19d4f44c9b0e5d30b545a54c4a659353
stefanobazzi <stefanobazzi@yahoo.it>
2014-06-10 12:31:32 +0200

    Removed dead code

--- client/comm_sys_tester.py
+++ client/comm_sys_tester.py
@@ -13,17 +13,16 @@ class TestCmdMessageClient(unittest.TestCase):
 		self.cmdmessageclient = CmdMessageClient('localhost', 6666)
 	
 	def test_packing_message(self):
-		LENGHT_FORMAT = '!i'
-		PACK_FORMAT = '{}89s'.format(LENGHT_FORMAT)
 		command_type = "syncronized"
 		param = {
 					"request" : {},
 					"respose" : {"yes" : 201, "no" : "300" ,}, 
 				}		
 		content = self.cmdmessageclient._packing_message(command_type, param)
-		data = struct.unpack(PACK_FORMAT, content)
-		data = json.loads(data[1])
-		
+		pack_format = '!i{}s'.format(len(content)-struct.calcsize('i'))
+		length, data = struct.unpack(pack_format, content)
+		data = json.loads(data)
+				
 
 if __name__ == '__main__':
 	unittest.main()
