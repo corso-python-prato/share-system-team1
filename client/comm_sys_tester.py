e2f0e19f207478c53560fcef5e556468d1457e96
stefanobazzi <stefanobazzi@yahoo.it>
2014-06-10 12:46:10 +0200

    Code clean

--- client/comm_sys_tester.py
+++ client/comm_sys_tester.py
@@ -8,21 +8,21 @@ import unittest
 
 
 class TestCmdMessageClient(unittest.TestCase):
-	
-	def setUp(self):
-		self.cmdmessageclient = CmdMessageClient('localhost', 6666)
-	
-	def test_packing_message(self):
-		command_type = "syncronized"
-		param = {
-					"request" : {},
-					"respose" : {"yes" : 201, "no" : "300" ,}, 
-				}		
-		content = self.cmdmessageclient._packing_message(command_type, param)
-		pack_format = '!i{}s'.format(len(content)-struct.calcsize('i'))
-		length, data = struct.unpack(pack_format, content)
-		data = json.loads(data)
-				
+
+    def setUp(self):
+        self.cmdmessageclient = CmdMessageClient('localhost', 6666)
+
+    def test_packing_message(self):
+        command_type = "syncronized"
+        param = {
+            "request": {},
+            "respose": {"yes": 201, "no": "300", },
+        }
+        content = self.cmdmessageclient._packing_message(command_type, param)
+        pack_format = '!i{}s'.format(len(content) - struct.calcsize('i'))
+        length, data = struct.unpack(pack_format, content)
+        data = json.loads(data)
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
