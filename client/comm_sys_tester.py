b74902d78561b9a1beff8c83be736170afac4ab9
stefanobazzi <stefanobazzi@yahoo.it>
2014-06-13 17:02:54 +0200

    Tester for packing and unpacking message works

--- client/comm_sys_tester.py
+++ client/comm_sys_tester.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from communicaton_system import CmdMessageClient
+from communicaton_system import CommunicatorSock
 import json
 import struct
 import unittest
@@ -10,19 +10,32 @@ import unittest
 class TestCmdMessageClient(unittest.TestCase):
 
     def setUp(self):
-        self.cmdmessageclient = CmdMessageClient('localhost', 6666)
-
-    def test_packing_message(self):
-        command_type = "syncronized"
-        param = {
+        self.communicator_sock = CommunicatorSock()
+        self.command_type = "syncronized"
+        self.param = {
             "request": {},
             "respose": {"yes": 201, "no": "300", },
         }
-        content = self.cmdmessageclient._packing_message(command_type, param)
+
+    def test_packing_message(self):
+        content = self.communicator_sock._packing_message(self.command_type, self.param)
         pack_format = '!i{}s'.format(len(content) - struct.calcsize('i'))
         length, data = struct.unpack(pack_format, content)
         data = json.loads(data)
 
-
+    def test_unpacking_message(self):
+        cmd_struct = {
+            'request': self.command_type,
+            'body': self.param,
+        }
+        cmd_struct = json.dumps(cmd_struct)
+        pack_size = len(cmd_struct)
+        data = struct.pack('!i', pack_size)
+        length = self.communicator_sock._unpacking_message(data)
+        pack_format = '!i{}s'.format(pack_size)
+        data = struct.pack(pack_format, pack_size, cmd_struct)
+        command = self.communicator_sock._unpacking_message(data, pack_format)
+      
+        
 if __name__ == '__main__':
     unittest.main()
