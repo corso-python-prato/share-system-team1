97c5f207f0ce2c535d47076a46601e647346a572
FedeDR <dellarovere.federico@gmail.com>
2014-06-13 12:10:05 +0200

    link client_cmdmanager to a communication system

--- client/client_cmdmanager.py
+++ client/client_cmdmanager.py
@@ -8,6 +8,10 @@ import sys
 import re
 import os
 
+from communicaton_system import CmdMessageClient
+import asyncore
+from client_daemon import load_config
+
 sys.path.insert(0, 'temp_mock/')
 import fakerequests as requests
 
@@ -22,9 +26,14 @@ class RawBoxCmd(cmd.Cmd):
 	prompt = Message().color('HEADER', '(RawBox) ')
 	ruler = Message().color('INFO','~')
 
-
+	def __init__(self):
+		cmd.Cmd.__init__(self)
+		conf = load_config()
+		self.comm_sock = CmdMessageClient(conf['cmd_host'], int(conf['cmd_port']), self)
+		
 	def _create_user(self, username = None):
 		"""create user if not exists"""
+		command_type = 'create_user'
 		  
 		if not username:
 			username  = raw_input('insert your user name: ')
@@ -44,35 +53,38 @@ class RawBoxCmd(cmd.Cmd):
 			Message('WARNING', 'invalid email')
 			email = raw_input('insert your user email: ')
 
-		user = 	{	
+		param = 	{	
 					'user': username, 
 					'psw': password, 
 					'email': email
 				}
 
-		r = requests.post("http://httpbin.org/post", data=user)
-		
-		if r.status_code == 201:
-			Message('SUCCESS','User created')
-		elif r.status_code == 409:
-			Message('WARNING','\nUser already exists\n')
-		elif r.status_code == 400:
-			Message('WARNING','\nIncorrect data format')
-		else:
-			Message('ERROR','Oops.. error ' + str(r.status_code) + ' please retry later')
-
+		self.comm_sock.send_message(command_type, param)
+		self.comm_sock.handle_read()
 
 	def _create_group(self, *args):
 		"""create group/s"""
-		print "create group/s ", args
+		command_type = 'create_group'
+		param = {'group': args}
+
+		self.comm_sock.send_message(command_type, param)
+		self.comm_sock.handle_read()
 
 	def _add_user(self, *args):
 		"""add user/s to a group """
-		print "add user/s to a group", args
+		command_type = 'add_to_group'
+		param = {'user': args}
+
+		self.comm_sock.send_message(command_type, param)
+		self.comm_sock.handle_read()
 
 	def _add_admin(self, *args):
 		"""add admin/s to a group """
-		print "add user/s to a group as admin/s", args
+		command_type = 'add_admin'
+		param = {'admin': args}
+
+		self.comm_sock.send_message(command_type, param)
+		self.comm_sock.handle_read()
 
 	def error(self, *args):
 		print "hum... unknown command, please type help"
@@ -116,6 +128,10 @@ class RawBoxCmd(cmd.Cmd):
 		""" exit from RawBox"""
 		if raw_input('[Exit] are you sure? y/n ') == 'y':
 			return True
+
+	def print_response(self, response):
+		print 'Response for "{}" command\nresult: {}'.format(response['request'],response['body'])
+
 
 def main():
 	if platform.system() == 'Windows':
