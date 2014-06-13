25ca75c7802e3ef4a1612d35bfa7039f64c8853f
stefanobazzi <stefanobazzi@yahoo.it>
2014-06-13 17:40:50 +0200

    Corrected test

--- client/comm_sys_tester.py
+++ client/comm_sys_tester.py
@@ -1,16 +1,15 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from communicaton_system import CommunicatorSock
-import json
+import communicaton_system
 import struct
 import unittest
+import json
 
 
 class TestCmdMessageClient(unittest.TestCase):
 
     def setUp(self):
-        self.communicator_sock = CommunicatorSock()
         self.command_type = "syncronized"
         self.param = {
             "request": {},
@@ -18,7 +17,7 @@ class TestCmdMessageClient(unittest.TestCase):
         }
 
     def test_packing_message(self):
-        content = self.communicator_sock._packing_message(self.command_type, self.param)
+        content = communicaton_system._packing_message(self.command_type, self.param)
         pack_format = '!i{}s'.format(len(content) - struct.calcsize('i'))
         length, data = struct.unpack(pack_format, content)
         data = json.loads(data)
@@ -31,10 +30,10 @@ class TestCmdMessageClient(unittest.TestCase):
         cmd_struct = json.dumps(cmd_struct)
         pack_size = len(cmd_struct)
         data = struct.pack('!i', pack_size)
-        length = self.communicator_sock._unpacking_message(data)
+        length = communicaton_system._unpacking_message(data)
         pack_format = '!i{}s'.format(pack_size)
         data = struct.pack(pack_format, pack_size, cmd_struct)
-        command = self.communicator_sock._unpacking_message(data, pack_format)
+        command = communicaton_system._unpacking_message(data, pack_format)
       
         
 if __name__ == '__main__':
