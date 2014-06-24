214f8bb58fbf9698bd601b002129b11be5071a7e
maggiof <francescomaggio11@gmail.com>
2014-06-24 13:03:58 +0200

    Upload, Download and Delete tests implemented

--- client/test_client_daemon.py
+++ client/test_client_daemon.py
@@ -9,51 +9,110 @@ import os
 
 
 class ClientDaemonTest(unittest.TestCase):
+
     def setUp(self):
         httpretty.enable()
         httpretty.register_uri(
             httpretty.POST,
-            'http://127.0.0.1:5000/API/v1/files/bla.txt',
+            'http://127.0.0.1:5000/API/v1/files/f_for_cdaemon_test.txt',
+            data={"response": "ok"})
+        httpretty.register_uri(
+            httpretty.PUT,
+            'http://127.0.0.1:5000/API/v1/files/f_for_cdaemon_test.txt',
+            data={"response": "ok"})
+        httpretty.register_uri(
+            httpretty.GET,
+            'http://127.0.0.1:5000/API/v1/files/f_for_cdaemon_test.txt',
+            body='[{"title": "Test"}]',
+            content_type="text/txt")
+        httpretty.register_uri(
+            httpretty.POST,
+            'http://127.0.0.1:5000/API/v1/actions/delete',
             data={"response": "ok"})
 
+
+        self.dir = "/tmp/home/monthly/daily"
+        file_name = "f_for_cdaemon_test.txt"
+        if not os.path.exists(self.dir):
+            os.makedirs(self.dir, 0755)
+        self.file_path = os.path.join(self.dir, file_name)
+        with open(self.file_path, 'w') as temp_file:
+                temp_file.write('test_file')
+        self.username = "usernameFarlocco"
+        self.password = "passwordSegretissima"
+        
     def tearDown(self):
         httpretty.disable()
         httpretty.reset()
     
-    def test_upload(self, put_file=False):
-        password = "passwordSegretissima"
-        username = "usernameFarlocco"
-        
-        file_path = '/Users/marc0/progetto/prove_deamon/bla.txt'
-        mock_file_content = open(file_path, 'r')
-
-        mock_auth_user = ":".join([username, password])
-        mock_data = 'asdasd'
+    def test_upload(self, put_file=True):
+        mock_auth_user = ":".join([self.username, self.password])
         client_daemon.ServerCommunicator(
             'http://127.0.0.1:5000/API/v1',
-            username,
-            password,
-            "/Users/marc0/progetto/prove_deamon").upload_file(file_path, put_file)
+            self.username,
+            self.password,
+            self.dir).upload_file(self.file_path, put_file)
         encoded = httpretty.last_request().headers['authorization'].split()[1]
         authorization_decoded = base64.decodestring(encoded)
-        #data = httpretty.last_request().parsed_body
         path = httpretty.last_request().path
         host = httpretty.last_request().headers['host']
         method = httpretty.last_request().method
         
         #check if authorization is equals
         self.assertEqual(authorization_decoded, mock_auth_user)
-        #check if data is equals
-        #self.assertEqual(data, mock_data)
         #check if url and method is equals
-
-        self.assertEqual(path, '/API/v1/files/bla.txt')
+        self.assertEqual(path, '/API/v1/files/f_for_cdaemon_test.txt')
         self.assertEqual(host, '127.0.0.1:5000')
         if put_file:
             self.assertEqual(method, 'PUT')
         else:
             self.assertEqual(method, 'POST')
 
+    def test_download(self):
+        mock_auth_user = ":".join([self.username, self.password])
+        response = client_daemon.ServerCommunicator(
+                        'http://127.0.0.1:5000/API/v1',
+                        self.username,
+                        self.password,
+                        self.dir).download_file(self.file_path)
+        encoded = httpretty.last_request().headers['authorization'].split()[1]
+        authorization_decoded = base64.decodestring(encoded)
+        path = httpretty.last_request().path
+        host = httpretty.last_request().headers['host']
+        method = httpretty.last_request().method
+
+        #check if authorization is equals
+        self.assertEqual(authorization_decoded, mock_auth_user)
+        #check if url and host is equals
+        self.assertEqual(path, '/API/v1/files/f_for_cdaemon_test.txt')
+        self.assertEqual(host, '127.0.0.1:5000')
+        #check if method equals
+        self.assertEqual(method, 'GET')
+        #check response's body
+        self.assertEqual(response[1], u'[{"title": "Test"}]')
+
+    def test_delete(self):
+        mock_auth_user = ":".join([self.username, self.password])
+        client_daemon.ServerCommunicator(
+                    'http://127.0.0.1:5000/API/v1',
+                    self.username,
+                    self.password,
+                    self.dir).delete_file(self.file_path)
+        encoded = httpretty.last_request().headers['authorization'].split()[1]
+        authorization_decoded = base64.decodestring(encoded)
+        path = httpretty.last_request().path
+        host = httpretty.last_request().headers['host']
+        method = httpretty.last_request().method
+
+        #check if authorization is equals
+        self.assertEqual(authorization_decoded, mock_auth_user)
+        #check if url and host is equals
+        self.assertEqual(path, '/API/v1/actions/delete')
+        self.assertEqual(host, '127.0.0.1:5000')
+        #check if method equals
+        self.assertEqual(method, 'POST')
+
+    """
     def init_snapshot(self):
         config = client_daemon.load_config()
         return client_daemon.DirSnapshotManager(config['dir_path'], config['snapshot_file_path'])
@@ -101,7 +160,7 @@ class ClientDaemonTest(unittest.TestCase):
 
         print "\n{:*^60}\n".format("\ndeamon internal conflicts == timestamp\n")
         snapshot_manager.syncronize_dispatcher(
-            server_timestamp=123123,
+            server_timestamp=file123123,
             server_snapshot=mock_snap_server)
 
         print "\n{:*^60}\n".format("\nno deamon internal conflicts != timestamp\n")
@@ -111,12 +170,12 @@ class ClientDaemonTest(unittest.TestCase):
 
     def diff_snapshot_paths(self):
         snapshot_manager = self.init_snapshot()
-        #mock_equal = """[u'/Users/marc0/progetto/prove_deamon/asdas/asdasd.txt', u'/Users/marc0/progetto/prove_deamon/asdas/Nuovo documento di testo.txt', u'/Users/marc0/progetto/prove_deamon/dsa.txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documento di testo (4).txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documentodi testo (3).txt', u'/Users/marc0/progetto/prove_deamon/bla.txt', u'/Users/marc0/progetto/prove_deamon/asdas/sdadsda.txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documento di testo (5).txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documento di testo.txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documento di testo (2).txt']"""
-        #mock_new_client = """[u'/Users/marc0/progetto/prove_deamon/asdas/bla.txt', u'/Users/marc0/progetto/prove_deamon/asd/gbla.txt', u'/Users/marc0/progetto/prove_deamon/asdas/gbla.txt']"""
-        #mock_new_server= """['path_farlocca']"""
+        #mock_equal = [u'/Users/marc0/progetto/prove_deamon/asdas/asdasd.txt', u'/Users/marc0/progetto/prove_deamon/asdas/Nuovo documento di testo.txt', u'/Users/marc0/progetto/prove_deamon/dsa.txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documento di testo (4).txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documentodi testo (3).txt', u'/Users/marc0/progetto/prove_deamon/bla.txt', u'/Users/marc0/progetto/prove_deamon/asdas/sdadsda.txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documento di testo (5).txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documento di testo.txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documento di testo (2).txt']
+        #mock_new_client = [u'/Users/marc0/progetto/prove_deamon/asdas/bla.txt', u'/Users/marc0/progetto/prove_deamon/asd/gbla.txt', u'/Users/marc0/progetto/prove_deamon/asdas/gbla.txt']
+        #mock_new_server= ['path_farlocca']
 
         snap_client = snapshot_manager.local_full_snapshot
-        snap_server = {
+        snap_server = {file
                     '9406539a103956dc36cb7ad35547198c': [u'/Users/marc0/progetto/prove_deamon\\bla.txt'],
                     'a8f5f167f44f4964e6c998dee827110c': [
                                                     u'/Users/marc0/progetto/prove_deamon\\asd\\gbla.txt',
@@ -143,6 +202,7 @@ class ClientDaemonTest(unittest.TestCase):
         #self.assertEqual(str(new_client), mock_new_client)
         #self.assertEqual(str(equal), mock_equal)
         #self.assertEqual(str(new_server), mock_new_server)
+    """
 
 if __name__ == '__main__':
     unittest.main()
