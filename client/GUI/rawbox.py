9df39622985f5e8d48ee30bbf628f1ac4ea3e5f6
Marco Pretelli <lordscapoladestra@gmail.com>
2014-06-25 22:14:07 +0200

    refactor init load status on gui

--- client/GUI/rawbox.py
+++ client/GUI/rawbox.py
@@ -1,8 +1,14 @@
 from ui_rawbox import Ui_Dialog
+from datetime import datetime
+from PyQt4 import QtCore
 from PyQt4 import QtGui
 import json
 import sys
 import os
+
+sys.path.insert(0, '../')
+from client_cmdmanager import *
+
 
 def get_dir_size(start_path = '.'):
     total_size = 0
@@ -18,21 +24,26 @@ def load_config():
     return config
 
 class Main(QtGui.QDialog, Ui_Dialog):
-    def __init__(self, parent=None):
+    def __init__(self, executer, config , parent=None):
         QtGui.QDialog.__init__(self, parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        self.dir_path = load_config()['dir_path']
+        self.dir_path = config['dir_path']
         self.max_size = 2000000000 #bytes
         self.ui.lineEdit_5.setText(self.dir_path)
         self.ui.progressBar.setValue((get_dir_size(self.dir_path) / float(self.max_size)) * 100)
         self.ui.label_5.setText("of: " + str(self.max_size/1000000000) + " GB")
+        self.executer = executer
+        self.load_status(config['snapshot_file_path'])
     
     def __del__ (self):
         self.ui = None
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
-    window = Main()
+    conf = load_config()
+    comm_sock = CmdMessageClient(conf['cmd_host'], int(conf['cmd_port']))
+    executer = RawBoxExecuter(comm_sock)
+    window = Main(executer, load_config())
     window.show()
     sys.exit(app.exec_())
