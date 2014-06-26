53824248ecfbdf15bbb76ed8b437bd856915c7ad
marcoPretelli <lordscapoladestra@gmail.com>
2014-06-26 09:23:47 +0200

    add scaffolding test syncronize dispatcher and syncronyze

--- client/test_client_daemon.py
+++ client/test_client_daemon.py
@@ -109,33 +109,58 @@ class ClientDaemonTest(unittest.TestCase):
 		config = client_daemon.load_config()
 		return client_daemon.DirSnapshotManager(config['dir_path'], config['snapshot_file_path'])
 	
+	def test_syncronize(self):
+		client_daemon.ServerCommunicator(
+			'http://127.0.0.1:5000/API/v1', 
+			'username', 
+			'password',
+			"/home/user/prove").synchronize("mock",self.init_snapshot())
+
 	def test_syncronize_dispatcher(self):
+
+		def my_global_md5():
+			return "52a0b3003d798aaec3be2e85dcf6d024"
+
 		snapshot_manager = self.init_snapshot()
+		snapshot_manager.global_md5 = my_global_md5
+		snapshot_manager.last_status = {"timestamp": 123123, "snapshot": "52a0b3003d798aaec3be2e85dcf6d024"}
+		snapshot_manager.local_full_snapshot = {
+		'9406539b203956dc36cb7ad35547198c': [u'/Users/marc0/progetto/prove_deamon\\bla.txt'],#new md5
+		'a8f5f167f44f4964e6c998dee827110c': [u'vecchio.txt'], 
+		'c21e1af364fa17cc80e0bbec2dd2ce5c': [u'/Users/marc0/progetto/prove_deamon\\asdas\\asdasd.txt'], 
+		'd41d8cd98f00b204e9899998ecf89999': [u'/Users/marc0/progetto/prove_deamon\\dsa.txt',#new md5 
+											u'/Users/marc0/progetto/prove_deamon\\Nuovo documento di testo (2).txt'],
+		'abcdefghilmnopqrstuvwyzabcdefghi': [u'new path client']}
+		
 		mock_snap_server = {
-		'9406539a103956dc36cb7ad35547198c': [u'/Users/marc0/progetto/prove_deamon\\bla.txt'],
-		'a8f5f167f44f4964e6c998dee827110c': [ u'/Users/marc0/progetto/prove_deamon\\asdas\\gbla.txt'], 'c21e1af364fa17cc80e0bbec2dd2ce5c': [u'/Users/marc0/progetto/prove_deamon\\asdas\\asdasd.txt'], 'd41d8cd98f00b204e9800998ecf8427e': [u'/Users/marc0/progetto/prove_deamon\\dsa.txt', u'/Users/marc0/progetto/prove_deamon\\Nuovo documento di testo (2).txt', u'/Users/marc0/progetto/prove_deamon\\Nuovo documento di testo (3).txt', u'/Users/marc0/progetto/prove_deamon\\Nuovo documento di testo (4).txt', u'/Users/marc0/progetto/prove_deamon\\Nuovo documento di testo (5).txt', u'/Users/marc0/progetto/prove_deamon\\Nuovo documento di testo.txt', u'/Users/marc0/progetto/prove_deamon\\asdas\\Nuovo documento di testo.txt', u'/Users/marc0/progetto/prove_deamon\\asdas\\sdadsda.txt',
-		u'path_farlocca'],'a8f5f167f44f4964e6c998dee827110b':[u'farlocco_2'],
-		'a8f5f167f44f491524c998dee827110c':[u'/Users/marc0/progetto/prove_deamon\\asdas\\bla.txt']}
+		'9406539a103956dc36cb7ad35547198c': [{"path": u'/Users/marc0/progetto/prove_deamon\\bla.txt',"timestamp":123123}],
+		'a8f5f167f44f4964e6c998dee827110c': [{"path": u'vecchio.txt',"timestamp":123122}], 
+		'c21e1af364fa17cc80e0bbec2dd2ce5c': [{"path": u'/Users/marc0/progetto/prove_deamon\\asdas\\asdasd.txt',"timestamp":123123}], 
+		'd41d8cd98f00b204e9800998ecf8427e': [{"path": u'/Users/marc0/progetto/prove_deamon\\dsa.txt',"timestamp":123122},#old timestamp 
+											{"path":  u'/Users/marc0/progetto/prove_deamon\\Nuovo documento di testo (2).txt',"timestamp":123123}, 
+											{"path":  u'server path in piu copiata',"timestamp":123123}],
+		'a8f5f167f44f4964e6c998dee827110b':[{"path":  u'nuova path server con md5 nuovo',"timestamp":123123}],
+		'a8f5f167f44f4964e6c998eee827110b':[{"path":  u'nuova path server con md5 nuovo e timestamp minore',"timestamp":123122}]}
 		
 		print "\n{:*^60}\n".format("\nno deamon internal conflicts == timestamp\n")
-		snapshot_manager.syncronize_dispatcher(
+		print snapshot_manager.syncronize_dispatcher(
 			server_timestamp = 123123,
 			server_snapshot = mock_snap_server)
 
 		print "\n{:*^60}\n".format("\nno deamon internal conflicts != timestamp\n")
-		snapshot_manager.syncronize_dispatcher(
+		print snapshot_manager.syncronize_dispatcher(
 			server_timestamp = 123124,
 			server_snapshot = mock_snap_server)
 		
 		snapshot_manager.last_status['snapshot'] = "21451512512512512"
 
 		print "\n{:*^60}\n".format("\ndeamon internal conflicts == timestamp\n")
-		snapshot_manager.syncronize_dispatcher(
+		print snapshot_manager.syncronize_dispatcher(
 			server_timestamp = 123123,
 			server_snapshot = mock_snap_server)
 
 		print "\n{:*^60}\n".format("\nno deamon internal conflicts != timestamp\n")
-		snapshot_manager.syncronize_dispatcher(
+		print snapshot_manager.syncronize_dispatcher(
 			server_timestamp = 123124,
 			server_snapshot = mock_snap_server)
 	def diff_snapshot_paths(self):
