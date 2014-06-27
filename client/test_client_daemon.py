4977e1cc96d044ad6e15d6af439187b7599c1609
FedeDR <dellarovere.federico@gmail.com>
2014-06-27 15:11:52 +0200

    fix for error typing

--- client/test_client_daemon.py
+++ client/test_client_daemon.py
@@ -51,12 +51,8 @@ class ServerCommunicatorTest(unittest.TestCase):
             'http://127.0.0.1:5000/API/v1/actions/copy')
         httpretty.register_uri(
             httpretty.POST,
-<<<<<<< HEAD
-            'http://127.0.0.1:5000/API/v1/create_user')
-=======
             'http://127.0.0.1:5000/API/v1/create_user',
             status=201)
->>>>>>> cd1a2b50ecb77df903b358206961fabb0c3b7a81
         httpretty.register_uri(httpretty.GET,
             'http://127.0.0.1:5000/API/v1/files',
             body=str({
@@ -218,86 +214,6 @@ class ServerCommunicatorTest(unittest.TestCase):
         self.server_comm._try_request = my_try_request
         self.server_comm.synchronize("mock")
 
-<<<<<<< HEAD
-=======
-        def my_global_md5():
-            return "52a0b3003d798aaec3be2e85dcf6d024"
-
-        snapshot_manager = self.init_snapshot()
-        snapshot_manager.global_md5 = my_global_md5
-        snapshot_manager.last_status = {"timestamp": 123123, "snapshot": "52a0b3003d798aaec3be2e85dcf6d024"}
-        snapshot_manager.local_full_snapshot = {
-                '9406539b203956dc36cb7ad35547198c': [u'/Users/marc0/progetto/prove_deamon\\bla.txt'],#new md5
-                'a8f5f167f44f4964e6c998dee827110c': [u'vecchio.txt'], 
-                'c21e1af364fa17cc80e0bbec2dd2ce5c': [u'/Users/marc0/progetto/prove_deamon\\asdas\\asdasd.txt'], 
-                'd41d8cd98f00b204e9899998ecf89999': [u'/Users/marc0/progetto/prove_deamon\\dsa.txt',#new md5 
-                                                     u'/Users/marc0/progetto/prove_deamon\\Nuovo documento di testo (2).txt'],
-                'abcdefghilmnopqrstuvwyzabcdefghi': [u'new path client']}
-        
-        mock_snap_server = {
-                '9406539a103956dc36cb7ad35547198c': [{"path": u'/Users/marc0/progetto/prove_deamon\\bla.txt',"timestamp":123123}],
-                'a8f5f167f44f4964e6c998dee827110c': [{"path": u'vecchio.txt',"timestamp":123122}], 
-                'c21e1af364fa17cc80e0bbec2dd2ce5c': [{"path": u'/Users/marc0/progetto/prove_deamon\\asdas\\asdasd.txt',"timestamp":123123}], 
-                'd41d8cd98f00b204e9800998ecf8427e': [{"path": u'/Users/marc0/progetto/prove_deamon\\dsa.txt',"timestamp":123122},#old timestamp 
-                                                    {"path":  u'/Users/marc0/progetto/prove_deamon\\Nuovo documento di testo (2).txt',"timestamp":123123}, 
-                                                    {"path":  u'server path in piu copiata',"timestamp":123123}],
-                'a8f5f167f44f4964e6c998dee827110b':[{"path":  u'nuova path server con md5 nuovo',"timestamp":123123}],
-                'a8f5f167f44f4964e6c998eee827110b':[{"path":  u'nuova path server con md5 nuovo e timestamp minore',"timestamp":123122}]}
-        
-        print "\n{:*^60}\n".format("\nno deamon internal conflicts == timestamp\n")
-        print snapshot_manager.syncronize_dispatcher(
-            server_timestamp = 123123,
-            server_snapshot = mock_snap_server)
-
-        print "\n{:*^60}\n".format("\nno deamon internal conflicts != timestamp\n")
-        print snapshot_manager.syncronize_dispatcher(
-            server_timestamp = 123124,
-            server_snapshot = mock_snap_server)
-        
-        snapshot_manager.last_status['snapshot'] = "21451512512512512"
-
-        print "\n{:*^60}\n".format("\ndeamon internal conflicts == timestamp\n")
-        print snapshot_manager.syncronize_dispatcher(
-            server_timestamp = 123123,
-            server_snapshot = mock_snap_server)
-
-        print "\n{:*^60}\n".format("\nno deamon internal conflicts != timestamp\n")
-        print snapshot_manager.syncronize_dispatcher(
-            server_timestamp = 123124,
-            server_snapshot = mock_snap_server)
-
-    def diff_snapshot_paths(self):
-        snapshot_manager = self.init_snapshot()
-        #mock_equal = [u'/Users/marc0/progetto/prove_deamon/asdas/asdasd.txt', u'/Users/marc0/progetto/prove_deamon/asdas/Nuovo documento di testo.txt', u'/Users/marc0/progetto/prove_deamon/dsa.txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documento di testo (4).txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documentodi testo (3).txt', u'/Users/marc0/progetto/prove_deamon/bla.txt', u'/Users/marc0/progetto/prove_deamon/asdas/sdadsda.txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documento di testo (5).txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documento di testo.txt', u'/Users/marc0/progetto/prove_deamon/Nuovo documento di testo (2).txt']
-        #mock_new_client = [u'/Users/marc0/progetto/prove_deamon/asdas/bla.txt', u'/Users/marc0/progetto/prove_deamon/asd/gbla.txt', u'/Users/marc0/progetto/prove_deamon/asdas/gbla.txt']
-        #mock_new_server= ['path_farlocca']
-
-        snap_client = snapshot_manager.local_full_snapshot
-        snap_server = {
-                    '9406539a103956dc36cb7ad35547198c': [u'/Users/marc0/progetto/prove_deamon\\bla.txt'],
-                    'a8f5f167f44f4964e6c998dee827110c': [u'/Users/marc0/progetto/prove_deamon\\asd\\gbla.txt',
-                                                        u'/Users/marc0/progetto/prove_deamon\\asdas\\bla.txt',
-                                                        u'/Users/marc0/progetto/prove_deamon\\asdas\\gbla.txt'],
-                    'c21e1af364fa17cc80e0bbec2dd2ce5c': [u'/Users/marc0/progetto/prove_deamon\\asdas\\asdasd.txt'],
-                    'd41d8cd98f00b204e9800998ecf8427e': [u'/Users/marc0/progetto/prove_deamon\\dsa.txt',
-                                                        u'/Users/marc0/progetto/prove_deamon\\Nuovo documento di testo (2).txt',
-                                                        u'/Users/marc0/progetto/prove_deamon\\Nuovo documento di testo (3).txt',
-                                                        u'/Users/marc0/progetto/prove_deamon\\Nuovo documento di testo (4).txt',
-                                                        u'/Users/marc0/progetto/prove_deamon\\Nuovo documento di testo (5).txt',
-                                                        u'/Users/marc0/progetto/prove_deamon\\Nuovo documento di testo.txt',
-                                                        u'/Users/marc0/progetto/prove_deamon\\asdas\\Nuovo documento di testo.txt',
-                                                        u'/Users/marc0/progetto/prove_deamon\\asdas\\sdadsda.txt', 
-                                                        u'path_farlocca']}
-        new_client, new_server, equal = snapshot_manager.diff_snapshot_paths(snap_client, snap_server)
-
-        
-         #new_client = str(new_client).replace('\\\\','/')
-         #new_server = str(new_server).replace('\\\\','/')
-         #equal = str(equal).replace('\\\\','/')
-
-         #self.assertEqual(str(new_client), mock_new_client)
-         #self.assertEqual(str(equal), mock_equal)
-         #self.assertEqual(str(new_server), mock_new_server)
 
 class FileSystemOperatorTest(unittest.TestCase):
 
@@ -367,8 +283,6 @@ class FileSystemOperatorTest(unittest.TestCase):
         self.file_system_op.delete_a_file(path_to_del)
         self.assertFalse(os.path.exists(path_to_del))
 
-
->>>>>>> cd1a2b50ecb77df903b358206961fabb0c3b7a81
 
 class DirSnapshotManagerTest(unittest.TestCase):
     def setUp(self):
