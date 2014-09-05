ed9acefff418f58755d55cb19fa0e04d3303df8a
Maggio Francesco <francescomaggio11@gmail.com>
2014-09-05 12:59:17 +0200

    Implemented set_password in case of not existent user, in test_server

--- server/test_server.py
+++ server/test_server.py
@@ -1256,6 +1256,17 @@ class UserActions(unittest.TestCase):
         response = self.tc.put(self.url2, data=data, headers=None)
         self.assertEqual(response.status_code, server.HTTP_ACCEPTED)
 
+    def test_set_password_not_existent_user(self):
+        data = {
+            "reset": True,
+            "code": UserActions.code,
+            "psw": UserActions.psw
+        }
+
+        self.inject_user(TEST_RESET_REQUESTS, UserActions.user, code=UserActions.code)
+        response = self.tc.put(self.url2, data=data, headers=None)
+        self.assertEqual(response.status_code, server.HTTP_ACCEPTED)
+
 if __name__ == '__main__':
     # TODO: these things, here, are ok for nose?
     server.app.config.update(TESTING=True)
